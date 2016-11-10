afbe1d75c624ea7c2074798dd017864fd6119196
aerosadegh <silverstar10@gmail.com>
2016-11-11 01:52:56 +0330

    change template/base.html.search form

--- helpy2/settings.py
+++ helpy2/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = "fa"
+LANGUAGE_BIDI = True
 
 # Supported languages
 LANGUAGES = (
